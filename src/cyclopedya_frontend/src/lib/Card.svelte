2906e1daa97c42dc4e6d1b064f28549a0ffc4685
SantiRendon <xantirendonc@gmail.com>
2024-06-10 17:05:45 -0500

    👔 card http outcall logic

--- src/cyclopedya_frontend/src/lib/Card.svelte
+++ src/cyclopedya_frontend/src/lib/Card.svelte
@@ -1,36 +1,98 @@
 <script>
-    let book = {
-    title : "Mort",
-    isbn: "9780552144292",
-    subjects: [ "Drama texts: from c 1900 -","General", "Fiction - Fantasy"],
-    cover: "./cover.jpg",
-    author: "Terry Pratchet",
-    first_sentence: "His is the bright candlelit room where the life-timers are stored-shelf",
-    info_url: "https://openlibrary.org/books/OL7815144M/Mort",
-    save_button: "function()"
-}
+    import { onMount } from "svelte";
+    import { backend } from "$lib/canisters";
 
+    export let isbn;
+    
+    let isLoading = false;
+
+    let book = {
+        title: "",
+        isbn: "",
+        subjects: "",
+        author: "",
+        preview: "",
+        info_url: "",
+        cover: "",
+    };
+
+    function resizeImageUrl(url) {
+        if (url.endsWith("-S.jpg")) {
+            return url.replace("-S.jpg", "-L.jpg");
+        } else {
+            return url;
+        }
+    }
+
+    async function fetchInfoBook(isbnValue) {
+        try {
+            isLoading = true;
+            const data = await backend.get_book_info(isbnValue);
+            return JSON.parse(data).docs[0];
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async function fetchUrlBook(isbnValue) {
+        try {
+            isLoading = true;
+            const data = await backend.get_book_urls(isbnValue);
+            return JSON.parse(data)[`ISBN:${isbn}`];
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    onMount(async () => {
+        let info = await fetchInfoBook(isbn);
+        let urls = await fetchUrlBook(isbn);
+
+        let newCover = resizeImageUrl(urls.thumbnail_url);
+
+        book = {
+            title: info.title,
+            isbn: isbn,
+            subjects: info.subject,
+            author: info.author_name,
+            preview: urls.preview_url,
+            info_url: urls.info_url,
+            cover: newCover,
+            // save_button: "function()",
+        };
+
+        isLoading = false;
+    });
 </script>
 
-<sl-card class="card-overview">
-    <img
-        slot="image"
-        src="https://images.unsplash.com/photo-1559209172-0ff8f6d49ff7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80"
-        alt="A kitten sits patiently between a terracotta pot and decorative grasses."
-    />
+{#if isLoading}
+    <sl-spinner style="font-size: 50px; --track-width: 10px;"></sl-spinner>
+{:else}
+    <sl-card class="card-overview">
+        <img
+            slot="image"
+            src={book.cover}
+            alt="A kitten sits patiently between a terracotta pot and decorative grasses."
+        />
 
-    <div style="text-align: center">
-        <strong>{book.title} - {book.isbn}</strong><br />
-        <p>{book.first_sentence}</p>
-        <p>{book.author}</p>
-        <small>{book.subjects[2]}</small>
-    </div>
+        <div style="text-align: center">
+            <strong>{book?.title} - {book?.isbn}</strong><br />
+            <!-- <p>{book?.preview}</p> -->
+            <p>{book?.author}</p>
+            <small>{book?.subjects[2]}</small>
+        </div>
 
-    <div slot="footer">
-        <sl-button pill variant="primary" href={book.info_url} target="blank">More Info</sl-button>
-        <sl-button pill variant="neutral">Save Book</sl-button>        
-    </div>
-</sl-card>
+        <div slot="footer">
+            <sl-button
+                pill
+                variant="primary"
+                href={book.info_url}
+                target="blank">More Info</sl-button
+            >
+            <sl-button pill variant="neutral">Save Book</sl-button>
+        </div>
+    </sl-card>
+{/if}
 
 <style>
     .card-overview {

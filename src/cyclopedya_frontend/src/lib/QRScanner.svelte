8945bbd56396043a267338bdfad1e0906d47551b
SantiRendon <xantirendonc@gmail.com>
2024-06-10 09:54:44 -0500

    👔 avance scanner

--- src/cyclopedya_frontend/src/lib/QRScanner.svelte
+++ src/cyclopedya_frontend/src/lib/QRScanner.svelte
@@ -1,93 +1,84 @@
 <script>
-  import { onMount } from 'svelte';
-  import Quagga from 'quagga';
+  import { onMount } from "svelte";
+  import { Html5QrcodeScanner } from "html5-qrcode";
+
+  let scanner;
+  let scannerValue;
+
+  let setScannerValue = (value) => {
+    scannerValue = decodedResult.result.text;
+    scanner.pause();
+    console.log(scannerValue);
+  };
+
+  const onScanSuccess = (decodedText, decodedResult) => {
+    // console.log(`Code matched = ${decodedText}`, decodedResult);
+    console.log(decodedResult.result.text);
+    scannerValue = decodedResult.result.text;
+    // scanner.pause();
+    // console.log(scannerValue);
+    // return `Code matched = ${decodedText?.decodedResult?.text} : decodedResult`
+    // Aquí puedes manejar el texto decodificado, por ejemplo, mostrarlo en la UI o realizar alguna acción.
+  };
+
+  const onScanFailure = (error) => {
+    // console.warn(`Code scan error = ${error}`);
+    // Puedes manejar los errores de escaneo aquí.
+  };
 
   onMount(() => {
-    const initQuagga = () => {
-      Quagga.init({
-        inputStream: {
-          name: "Live",
-          type: "LiveStream",
-          target: document.querySelector('#reader'),
-          constraints: {
-            width: { min: 640 },
-            height: { min: 480 },
-            facingMode: "environment", // Trata de usar la cámara trasera en dispositivos móviles
-          },
-        },
-        decoder: {
-          readers: ["ean_reader", "ean_8_reader", "ean_13_reader"], // Puedes agregar más lectores si es necesario
-        },
-        locate: true,
-      }, (err) => {
-        if (err) {
-          console.error("Error al inicializar Quagga:", err);
-          return;
-        }
-        console.log("Initialization finished. Ready to start");
-        Quagga.start();
-      });
+    scanner = new Html5QrcodeScanner(
+      "reader",
+      {
+        fps: 10,
+        qrbox: { width: 450, height: 450 },
+        rememberLastUsedCamera: true,
+        formatsToSupport: [
+          0, // QR_CODE
+          1, // AZTEC
+          2, // CODABAR
+          3, // CODE_39
+          4, // CODE_93
+          5, // CODE_128
+          6, // DATA_MATRIX
+          7, // MAXICODE
+          8, // ITF
+          9, // EAN_13
+          10, // EAN_8
+          11, // PDF_417
+          12, // RSS_14
+          13, // RSS_EXPANDED
+          14, // UPC_A
+          15, // UPC_E
+          16, // UPC_EAN_EXTENSION
+        ],
+      },
+      // verbose= false,
+    );
 
-      Quagga.onProcessed((result) => {
-        const drawingCtx = Quagga.canvas.ctx.overlay;
-        const drawingCanvas = Quagga.canvas.dom.overlay;
+    if (scannerValue) {
+      scanner.pause();
+      console.log(scannerValue);
+    }
 
-        // Configuración del contexto del canvas para mejorar el rendimiento
-        drawingCanvas.getContext('2d', { willReadFrequently: true });
-
-        if (result) {
-          drawingCtx.clearRect(0, 0, drawingCanvas.width, drawingCanvas.height);
-
-          if (result.boxes) {
-            result.boxes.filter(box => box !== result.box).forEach(box => {
-              Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, { color: "green", lineWidth: 2 });
-            });
-          }
-
-          if (result.box) {
-            Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, { color: "#00F", lineWidth: 2 });
-          }
-
-          if (result.codeResult && result.codeResult.code) {
-            Quagga.ImageDebug.drawPath(result.line, { x: 'x', y: 'y' }, drawingCtx, { color: 'red', lineWidth: 3 });
-          }
-        }
-      });
-
-      Quagga.onDetected((result) => {
-        const code = result.codeResult.code;
-        console.log("Barcode detected and processed: [" + code + "]", result);
-        // Aquí puedes manejar el código decodificado
-      });
-    };
-
-    navigator.mediaDevices.getUserMedia({ video: true })
-      .then((stream) => {
-        // Permisos de cámara concedidos
-        initQuagga();
-      })
-      .catch((err) => {
-        console.error("No se pudo acceder a la cámara: ", err);
-      });
+    scanner.render(onScanSuccess, onScanFailure);
 
     return () => {
-      Quagga.stop();
+      scanner.clear().catch((error) => {
+        console.error("Error cleaning up scanner", error);
+      });
     };
   });
 </script>
 
+<div id="reader"></div>
+
 <style>
   #reader {
-    width: 100%;
-    max-width: 640px;
-    height: 480px;
+    width: 250px;
+    height: 250px;
     margin: auto;
     border: 2px solid black;
     border-radius: 10px;
-    position: relative;
-    z-index: 10;
-    margin-bottom: 50px;
   }
 </style>
-
-<div id="reader"></div>
